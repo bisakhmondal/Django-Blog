2d5761d7ccaff80b61c91287cc8b36e3827a9025
Bisakh Mondal <bisakhmondal00@gmail.com>
2020-01-12 13:05:48 +0530

    email validation on reset password

--- blog_with_django/urls.py
+++ blog_with_django/urls.py
@@ -20,14 +20,14 @@ from django.contrib.auth import views as auth_views
 #https://docs.djangoproject.com/en/3.0/howto/static-files/
 from django.conf import settings
 from django.conf.urls.static import static
-
+from users.forms import EmailValidationOnForgotPassword
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_views.register,name='register'),
     path('profile/',user_views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
-    path('reset-password/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset'),
+    path('reset-password/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html',form_class=EmailValidationOnForgotPassword),name='password_reset'),
     path('reset-password/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
     path('reset-password-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
     path('reset-password-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,PasswordResetForm
 from .models import profile
 class UserRegisterForm(UserCreationForm):
     email=forms.EmailField() #by default you have to fill it else put required=false
@@ -19,3 +19,13 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model=profile
         fields=['image'] 
+
+
+class EmailValidationOnForgotPassword(PasswordResetForm):
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not User.objects.filter(email__iexact=email, is_active=True).exists():
+            msg = "There is no user registered with the specified E-Mail address."
+            self.add_error('email',msg)
+        return email
